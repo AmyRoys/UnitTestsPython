0985d935594a2eca0c26261a47a350ce90fdfeb5
AmyRoys <114415869+AmyRoys@users.noreply.github.com>
2023-05-06 19:58:37 +0100

    Mock dependencies in the test

--- test_shopping_cart.py
+++ test_shopping_cart.py
@@ -1,9 +1,21 @@
+from unittest.mock import Mock
+
+import item_database
+from item_database import ItemDatabase
 from shopping_cart import ShoppingCart
 import pytest
 
+
 @pytest.fixture
+# setup for the cart, reduces code duplication
 def cart():
     return ShoppingCart(5)
+
+
+@pytest.fixture
+def database():
+    return ItemDatabase()
+
 
 def test_can_add_item_to_cart(cart):
     cart.add("apple")
@@ -11,20 +23,23 @@ def test_can_add_item_to_cart(cart):
     assert cart.size() == 1
 
 
-def test_can_get_items(cart):
+def test_can_get_total_price(cart, database):
     cart.add("apple")
     cart.add("orange")
 
-    price_map = {
-        "apple": 1.0,
-        "orange": 2.0,
-    }
-    assert cart.get_total_price(price_map) == 3.0
+    # price_map = {
+    #     "apple": 1.0,
+    #     "orange": 2.0,
+    # }
+    # replace instance of item database with price map
+    def mock_get_item(item: str):
+        if item == "apple":
+            return 1.0
+        if item == "orange":
+            return 2.0
 
-
-def test_can_get_total_price(cart):
-    print("Testing can get price")
-    pass
+    item_database.get = Mock(side_effect=mock_get_item())
+    assert cart.get_total_price(item_database) == 3.0
 
 
 def test_when_item_added_cart_contains_item(cart):
