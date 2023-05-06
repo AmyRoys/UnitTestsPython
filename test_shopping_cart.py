0432651f6b6412cdfd8c5b9bdd972b2af23fc148
AmyRoys <114415869+AmyRoys@users.noreply.github.com>
2023-05-06 19:17:02 +0100

    Add max of 5 items to cart Add Overflow error if over max - passes

--- test_shopping_cart.py
+++ test_shopping_cart.py
@@ -1,10 +1,32 @@
 from shopping_cart import ShoppingCart
+import pytest
+
 def test_can_add_item_to_cart():
-    cart = ShoppingCart()
+    cart = ShoppingCart(5)
     cart.add("apple")
 
     assert cart.size() == 1
+
+
 def test_can_get_items():
     pass
+
+
 def test_can_get_total_price():
     pass
+
+
+def test_when_item_added_cart_contains_item():
+    cart = ShoppingCart(5)
+    cart.add("apple")
+    assert "apple" in cart.get_items()
+
+
+def test_when_cart_is_more_than_max():
+    cart = ShoppingCart(5)
+    with pytest.raises(OverflowError):
+        for i in range(6):
+            cart.add("apple")
+
+    pass
+
